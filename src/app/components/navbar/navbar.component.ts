393096245f2cc04f9278b55f555d0d2c43614822
Jacob Tornes <jacob.emil.tornes@no.experis.com>
2023-03-03 15:58:45 +0100

    update: navbar components

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  get user(): User | undefined {
+    return this.userService.user;
+  }
+
+  constructor(private readonly userService: UserService) {}
+
+  ngOnInit(): void {
+
+  }
 
 }
