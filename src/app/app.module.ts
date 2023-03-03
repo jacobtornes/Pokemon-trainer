400bbf808528df96ed73453d4b21195f164f2fb4
Jacob Tornes <jacob.emil.tornes@no.experis.com>
2023-03-03 15:29:53 +0100

    feat: add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LoginFormComponent } from './components/login-form/login-form.component
 import { FormsModule } from '@angular/forms';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PokemonListItemComponent } from './components/pokemon-list-item/pokemon-list-item.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { PokemonListItemComponent } from './components/pokemon-list-item/pokemon
     LoginFormComponent,
     PokemonListComponent,
     PokemonListItemComponent,
+    NavbarComponent,
   ],
   imports: [
     // Modules!
